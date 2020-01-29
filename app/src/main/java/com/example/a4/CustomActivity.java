b819c434f3f53c5bc60e302435f81d878a598e34
iEmix <emdom4@gmail.com>
2020-01-29 23:01:30 +0100

    Przechodzenie do nowych aktywnosci swipem

---
+++ app/src/main/java/com/example/a4/CustomActivity.java
@@ -0,0 +1,10 @@
+package com.example.a4;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noname);
+    }
+}
